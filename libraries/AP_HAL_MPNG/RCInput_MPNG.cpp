b66e8a68d93a4ae287f1dc8b720125ce0eb4c5a0
SirAlex <rsoft@tut.by>
2013-10-10 14:52:43 +0300

    Removed compilation hints

--- libraries/AP_HAL_MPNG/RCInput_MPNG.cpp
+++ libraries/AP_HAL_MPNG/RCInput_MPNG.cpp
@@ -14,7 +14,7 @@ using namespace MPNG;
 extern const HAL& hal;
 
 // PPM_SUM(CPPM) or PWM Signal processing
-#define SERIAL_PPM SERIAL_PPM_DISABLED
+#define SERIAL_PPM SERIAL_PPM_ENABLED
 /*
 	SERIAL_PPM_DISABLED				// Separated channel signal (PWM) on A8-A15 pins
 	SERIAL_PPM_ENABLED				// For all boards, PPM_SUM pin is A8
@@ -210,38 +210,46 @@ void MPNGRCInput::_pwm_A8_A15_isr(void)
 	// mask is pins [D0-D7] that have changed // the principle is the same on the MEGA for PORTK and [A8-A15] PINs
 	// chan = pin sequence of the port. chan begins at D2 and ends at D7
 	
-	if (mask & 1<<0)
+	if (mask & 1<<0) {
 		if (!(pin & 1<<0)) {
 			dTime = (cTime-edgeTime[0]); if (MIN_PULSEWIDTH<dTime && dTime<MAX_PULSEWIDTH) _pulse_capt[0] = dTime;
 		} else edgeTime[0] = cTime;
-	if (mask & 1<<1)
+	}
+	if (mask & 1<<1) {
 		if (!(pin & 1<<1)) {
 			dTime = (cTime-edgeTime[1]); if (MIN_PULSEWIDTH<dTime && dTime<MAX_PULSEWIDTH) _pulse_capt[1] = dTime;
 		} else edgeTime[1] = cTime;
-	if (mask & 1<<2) 
+	}
+	if (mask & 1<<2) {
 		if (!(pin & 1<<2)) {
 			dTime = (cTime-edgeTime[2]); if (MIN_PULSEWIDTH<dTime && dTime<MAX_PULSEWIDTH) _pulse_capt[2] = dTime;
 		} else edgeTime[2] = cTime;
-	if (mask & 1<<3)
+	}
+	if (mask & 1<<3) {
 		if (!(pin & 1<<3)) {
 			dTime = (cTime-edgeTime[3]); if (MIN_PULSEWIDTH<dTime && dTime<MAX_PULSEWIDTH) _pulse_capt[3] = dTime;
 		} else edgeTime[3] = cTime;
-	if (mask & 1<<4) 
+	}
+	if (mask & 1<<4) {
 		if (!(pin & 1<<4)) {
 			dTime = (cTime-edgeTime[4]); if (MIN_PULSEWIDTH<dTime && dTime<MAX_PULSEWIDTH) _pulse_capt[4] = dTime;
 		} else edgeTime[4] = cTime;
-	if (mask & 1<<5)
+	}
+	if (mask & 1<<5) {
 		if (!(pin & 1<<5)) {
 			dTime = (cTime-edgeTime[5]); if (MIN_PULSEWIDTH<dTime && dTime<MAX_PULSEWIDTH) _pulse_capt[5] = dTime;
 		} else edgeTime[5] = cTime;
-	if (mask & 1<<6)
+	}
+	if (mask & 1<<6) {
 		if (!(pin & 1<<6)) {
 			dTime = (cTime-edgeTime[6]); if (MIN_PULSEWIDTH<dTime && dTime<MAX_PULSEWIDTH) _pulse_capt[6] = dTime;
 		} else edgeTime[6] = cTime;
-	if (mask & 1<<7)
+	}
+	if (mask & 1<<7) {
 		if (!(pin & 1<<7)) {
 			dTime = (cTime-edgeTime[7]); if (MIN_PULSEWIDTH<dTime && dTime<MAX_PULSEWIDTH) _pulse_capt[7] = dTime;
 		} else edgeTime[7] = cTime;
+	}
 	
 	// If we got pulse on throttle pin, report success  
 	if (mask & 1<<pinRcChannel[2]) {
