eb10d83769a527568868d2ff474eac668969e762
SirAlex <rsoft@tut.by>
2013-12-16 00:05:28 +0300

    Allow to pass firmware version from BuildServer
    Use default Board Type (prevents to use different boards in FlashTool)

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -3,7 +3,7 @@
 // User specific config file.  Any items listed in config.h can be overridden here.
 
 // Select Megapirate board type:
-#define MPNG_BOARD_TYPE   RCTIMER_CRIUS_V2
+//#define MPNG_BOARD_TYPE   CRIUS_V1
 /*
   RCTIMER_CRIUS_V2    -- (DEFAULT!!!) Use ONLY for RCTimer CRIUS V2 board
   CRIUS_V1            -- Use this define for RCTimer CRIUS V1(1.1) board and all HobbyKing AIOP boards
--- ArduCopter/ArduCopter.pde
+++ ArduCopter/ArduCopter.pde
@@ -1,6 +1,7 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
-
-#define THISFIRMWARE "ArduCopter-MPNG V3.0.1 R3b"
+#ifndef THISFIRMWARE
+#  define THISFIRMWARE "ArduCopter-MPNG V3.0.1 R3b"
+#endif
 /*
  *  ArduCopter Version 3.0
  *  Creator:        Jason Short
