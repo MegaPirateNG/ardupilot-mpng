05d9a5e4cb7f1f2a25a088610a248c0b9dd827e1
Maxim Strinzha <mstrinzha@gmail.com>
2014-04-14 13:04:17 +0400

    F4BY HAL

---
+++ libraries/AP_HAL_F4BY/AP_HAL_PX4_Namespace.h
@@ -0,0 +1,19 @@
+
+#ifndef __AP_HAL_PX4_NAMESPACE_H__
+#define __AP_HAL_PX4_NAMESPACE_H__
+
+namespace PX4 {
+	class PX4Scheduler;
+	class PX4UARTDriver;
+	class PX4Storage;
+	class PX4RCInput;
+	class PX4RCOutput;
+	class PX4AnalogIn;
+	class PX4AnalogSource;
+	class PX4Util;
+        class PX4GPIO;
+        class PX4DigitalSource;
+}
+
+#endif //__AP_HAL_PX4_NAMESPACE_H__
+
